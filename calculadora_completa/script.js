e514fc7e5a66dce887c769a213929b11da79b501
matheus-cmc <matheusfeira@hotmail.com>
2025-10-17 12:17:12 +0000

    calculadora

--- calculadora_completa/script.js
+++ calculadora_completa/script.js
@@ -1,12 +1,12 @@
 const display = document.getElementById('display');
 
-const botoes = document.querySelectorAll('.botao, .maior, .menor');
+const botoes = document.querySelectorAll('.botao, .maior, .menor, .a');
 
 botoes.forEach(botao => {
     botao.addEventListener('click', () =>{
         if(botao.textContent ==='C'){
             display.value='';
-        }
+        } 
 
         else if(botao.textContent==='DEL'){
             display.value=display.value.slice(0, -1);
@@ -15,8 +15,12 @@ botoes.forEach(botao => {
 
         else if(botao.textContent==='='){
             try{
+                if(display.value !== ''){
+                    console.log('Calculando', display.value);
                 display.value=eval(display.value);
+                }
             }catch (e){
+                console.error("Erro ao calcular:", e)
                 display.value="Erro";
             }
 
